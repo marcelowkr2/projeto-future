ce725cee9255e0d1024c80f2977403839b506fec
Marcelo Pires de Oliveira <marcelowkr@hotmail.com>
2025-03-20 11:33:13 -0300

    update

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,18 +3,21 @@ from django.db import models
 
 # Gerenciador Personalizado para o CustomUser
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
+    def create_user(self, username, email=None, password=None, **extra_fields):
         if not username:
             raise ValueError("O campo 'username' é obrigatório")
-        user = self.model(username=username, **extra_fields)
+        if not email:
+            raise ValueError("O campo 'email' é obrigatório")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        return self.create_user(username, password, **extra_fields)
+        return self.create_user(username, email, password, **extra_fields)
 
 # Modelo Personalizado de Usuário
 class CustomUser(AbstractUser):
@@ -25,6 +28,7 @@ class CustomUser(AbstractUser):
         ('user', 'Usuário Comum'),
     ]
 
+    email = models.EmailField(unique=True, blank=False)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='user')
 
     # Evitar conflitos com o related_name
@@ -60,6 +64,7 @@ class Report(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reports')
 
     def __str__(self):
         return self.title
